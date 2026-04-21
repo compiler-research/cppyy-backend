b11bb977d149baa6f6f2f0a2ae99b2c7b4e165d1
Vassil Vassilev <v.g.vassilev@gmail.com>
2026-04-21 17:05:52 +0000

    Teach the backend to work with the experimental tablegen

--- clingwrapper/src/cppinterop_dispatch.cxx
+++ clingwrapper/src/cppinterop_dispatch.cxx
@@ -1,5 +1,11 @@
+#if __has_include("CppInterOp/CppInterOpAPI.inc")
+using namespace CppImpl;
+#define CPPINTEROP_API_FUNC(DN, CN, Ret, DeclArgs, CallArgs, RawTypes)  \
+  Ret (*CppInternal::DispatchRaw::DN) RawTypes = nullptr;
+#include "CppInterOp/CppInterOpAPI.inc"
+#else
 #include <CppInterOp/Dispatch.h>
-
 #define DISPATCH_API(name, type) CppAPIType::name Cpp::name = nullptr;
 CPPINTEROP_API_TABLE
 #undef DISPATCH_API
+#endif
